9d76830aa41071c41c41f8659f9a6d0b174c0cc0
Tuhin Kumar Nandi <rijustar1998@gmail.com>
2026-06-17 09:09:39 +0530

    changes

--- src/main/java/com/tuhinK/eCommerce/commons/models/BaseModel.java
+++ src/main/java/com/tuhinK/eCommerce/commons/models/BaseModel.java
@@ -7,17 +7,11 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
+@Setter
+@Getter
 @MappedSuperclass
 public class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
